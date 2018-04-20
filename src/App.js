ac4327f886e28c499e00c5b08c9cb9350597b0cd
Bruce Leong <brucewleong@gmail.com>
2018-04-20 11:38:14 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import profile from './profile_pic.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bruce's website</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header>
+      <h1 id="title">Bruce Leong</h1>
+      <p id="kicker"> Creator // Runner // Web Developer</p>
+    </header>
+
+    <nav>
+      <a href="">About Me</a>
+      <a href="https://biscotini917.github.io/GuessingGame-Part3/ ">Work</a>
+      <a href="">Clients</a>
+      <a href="mailto:brucewleong@gmail.com?Subject=Inquiry" target="_top">Send Mail</a>
+    </nav>
+    <article id="about_me">
+    <img id="profile_image" src={profile} />
+    <h1>Who is Bruce?</h1>
+    <p>Runner, Web Developer, lives in Astoria, father of 2 cats</p>
+  </article>
       </div>
     );
   }
