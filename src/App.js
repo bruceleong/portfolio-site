7ab2484d08f5d9b14c56c4c3d2612b2c49bc0aeb
Bruce Leong <brucewleong@gmail.com>
2018-04-29 21:53:46 -0400

    added dg + samurai cat

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import profile from './profile_pic.png'
+import sCatCover from './images/scatCover.png'
+import sCatGame from './images/ezgif.com-gif-maker.gif'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
   render() {
+    console.log(this.state, 'current state')
     return (
       <div className="App">
-      <header>
-      <h1 id="title">Bruce Leong</h1>
-      <p id="kicker"> Creator // Runner // Web Developer</p>
-    </header>
+        <header>
+          <h1 id="title">Bruce Leong</h1>
+          <p id="kicker"> Creator // Runner // Web Developer</p>
+        </header>
 
-    <nav>
-    <div id='stuff'>
-      <a href="">About</a>
-      <a href="https://biscotini917.github.io/GuessingGame-Part3/ ">Work</a>
-      <a href="">Clients</a>
-      <a href="mailto:brucewleong@gmail.com?Subject=Inquiry" target="_top">Send Mail</a>
-      </div>
-    </nav>
-    <article id="about_me">
-    <img id="profile_image" src={profile} />
-    <h1>Who is Bruce?</h1>
-    <p>Runner, Web Developer, lives in Astoria, father of 2 cats</p>
-  </article>
+        <nav>
+          <div>
+            <a href="mailto:brucewleong@gmail.com?Subject=Yo Buddy" target="_top">Contact Bruce</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/open?id=10UlFQ0tGuf4nDC1Zjf1ESHWtmpzW-teP">Checkout Bruce's Resume</a>
+          </div>
+        </nav>
+        <div className="whitePage">
+          <img id="profile_image" src={profile} />
+          <div className="textContent">
+            <h1>Who is Bruce?</h1>
+            <p>Bruce Leong is a Runner, Software Developer, father of 2 cats</p>
+            <p>He is a tech veteran with experience at top tier companies like Pandora and WebMD</p>
+            <p>He launched the <a target="_blank" rel="noopener noreferrer" href="https://www.dapperguide.com">DapperGuide Magazine</a></p>
+            <p>See more details below</p>
+          </div>
+        </div>
+        <div className="greyPage">
+          <h1>Bruce's Work</h1>
+          <h2>Articles: </h2>
+          <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@brucewleong/building-a-game-with-phaserjs-easy-steps-7e4b38d36e22"><h4>Build a game with PhaserJS (Easy steps) - Publication: Medium 2018</h4></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://dapperguide.com"><h4>Dapper Guide Magazine</h4></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://bruceleong.github.io/SamuraiCat/" style={{ textDecoration: 'none' }}>
+            <div>
+              <div className="textContent">
+                <h2>Samurai Cat Game</h2>
+                <p>Description: Samurai Cat is a platformer game created during a Fullstack Academy Hackathon, which took place over the course of 4 days where developers were invited to explore and create new products</p>
+                <p>Technologies: Javascript, Phaser, Trello, designed all artwork</p>
+                <h4>CLICK HERE TO PLAY!</h4>
+              </div>
+              <iframe width="336" height="200" src="https://www.youtube.com/embed/wYe5ICYOUNU" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+            </div>
+            <img className="sCat" src={sCatCover} />
+            <img className="sCat" src={sCatGame} />
+          </a>
+        </div>
       </div>
     );
   }
