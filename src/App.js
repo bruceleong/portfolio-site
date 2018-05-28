6cd3b333f2dd9c9f8e6f54067e16acdd49430e10
Bruce Leong <brucewleong@gmail.com>
2018-05-27 23:26:30 -0400

    fixed typos

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
         <nav>
           <div>
             <a href="mailto:brucewleong@gmail.com?Subject=Yo Buddy" target="_top">Contact Bruce</a>
-            <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/open?id=10UlFQ0tGuf4nDC1Zjf1ESHWtmpzW-teP">Checkout Bruce's Resume</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/open?id=10UlFQ0tGuf4nDC1Zjf1ESHWtmpzW-teP">Check out Bruce's Resume</a>
           </div>
         </nav>
         <div className="whitePage">
@@ -33,19 +33,19 @@ class App extends Component {
             <p>Bruce Leong is a Runner, Software Engineer, father of 2 cats</p>
             <p>He is a tech veteran with experience at top tier companies like Pandora and WebMD</p>
             <p>He launched the <a target="_blank" rel="noopener noreferrer" href="https://www.dapperguide.com">Dapper Guide Magazine</a></p>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/bruce-leong-112a904/"><p>Checkout his Linkedin</p></a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/bruce-leong-112a904/"><p>Check out his Linkedin</p></a>
             <p>See more details below</p>
           </div>
         </div>
         <div className="greyPage">
           <h1>Bruce's Work</h1>
-          <h2>Code: </h2>
+          <h2>His Code: </h2>
           <a target="_blank" rel="noopener noreferrer" href="https://github.com/bruceleong"><img style={{width: '25%', height: 'auto'}} src={github} /></a>
-          <h2>Writing: </h2>
+          <h2>His Writing: </h2>
           <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@brucewleong/building-a-game-with-phaserjs-easy-steps-7e4b38d36e22"><h4>Build a game with PhaserJS (Easy steps) - Publication: Medium 2018</h4></a>
           <a target="_blank" rel="noopener noreferrer" href="https://dapperguide.com"><h4>Dapper Guide Magazine</h4></a>
           <br />
-          <h2>Projects: </h2>
+          <h2>His Projects: </h2>
           <a target="_blank" rel="noopener noreferrer" href="https://bruceleong.github.io/SamuraiCat/" style={{ textDecoration: 'none' }}>
             <div>
               <div className="textContent">
